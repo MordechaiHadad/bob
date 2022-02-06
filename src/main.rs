ab6883c4ad426d7679b60d497165cb3f817e2ea3
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-02-06 19:34:15 +0200

    Added some error handling and WIP file downloading.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+extern crate core;
+
 use std::fs::File;
+use std::io::{Error, ErrorKind};
 use std::num::ParseFloatError;
 use serde::{Serialize, Deserialize};
-use std::process::Command;
+use std::process::{Command, exit};
 use clap::{App, Arg, arg, ArgMatches};
 use regex::Regex;
 use tokio::io::AsyncWriteExt;
@@ -17,20 +20,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if let Some(subcommand) = app.subcommand_matches("use") {
         if let Some(value) = subcommand.value_of("VERSION") {
             let version = String::from(value);
-            let version = parse_version(version).await?;
-            download_version(&version).await?;
+            let version = match parse_version(version).await {
+                Ok(value) => value,
+                Err(error) => {
+                    eprintln!("Error: {}", error.as_ref());
+                    exit(1);
+                }
+            };
+           if let Err(error) = download_version(&version).await { eprintln!("Error: {}", error.as_ref()); exit(1); }
 
         }
     }
     Ok(())
 }
+async fn parse_version(mut version: String) -> Result<String, Box<dyn std::error::Error>> {
+    match version.as_str() {
+        "nightly" | "stable" => Ok(version),
+        _ => {
+            let regex = Regex::new(r"[0-9]*\.[0-9]*\.[0-9]*").unwrap();
+            if regex.is_match(&*version) {
+                if !version.contains("v") { version = format!("v{}", version);}
+                return Ok(version)
+            }
+            Err(Box::new(Error::new(ErrorKind::Other, "Please provide a proper version string")))
+        }
+    }
+}
 
 async fn download_version(version: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let client = reqwest::Client::new();
-    let response = client.get(format!("https://github.com/neovim/neovim/releases/download/{}/nvim-win64.zip", version))
-        .header("user-agent", "bob")
-        .send()
-        .await;
+    let response = send_request(version).await;
 
     match response {
         Ok(response) => {
@@ -39,23 +57,35 @@ async fn download_version(version: &str) -> Result<(), Box<dyn std::error::Error
                 let mut file = tokio::fs::File::create("bro.zip").await?;
                 file.write_all(&response_bytes).await;
                 println!("Successfully downloaded version {}", version);
-            } else {println!("Please provide an existing version");}
+                Ok(())
+            } else { Err(Box::new(Error::new(ErrorKind::Other, "Please provide an existing neovim version"))) }
         },
-        Err(error) => println!("{}", error)
+        Err(error) => Err(Box::new(Error::new(ErrorKind::Other, error)))
     }
-    Ok(())
 }
 
-async fn parse_version<'a>(mut version: String) -> Result<String, &'a str> {
-    match &*version {
-        "nightly" | "stable" => Ok(version),
-        _ => {
-            let regex = Regex::new(r"[0-9]*\.[0-9]*\.[0-9]*").unwrap();
-            if regex.is_match(&*version) {
-                if !version.contains("v") { version = format!("v{}", version);}
-                return Ok(version)
-            }
-            Err("Please provide a proper version string")
-        }
+async fn send_request(version: &str) -> Result<reqwest::Response, reqwest::Error> {
+
+    let os = if cfg!(target_os = "linux") {
+        "linux64"
+    } else if cfg!(target_os = "windows") {
+        "win64"
+    } else {"macos"};
+    let request_url = format!("https://github.com/neovim/neovim/releases/download/{}/nvim-{}.{}", version, os, get_file_type().await);
+
+    let client = reqwest::Client::new();
+    client.get(request_url)
+        .header("user-agent", "bob")
+        .send()
+        .await
+}
+
+async fn get_file_type() -> String {
+    if cfg!(target_family = "windows") {
+        String::from("zip")
+    } else {
+        String::from("tar.gz")
     }
 }
+
+
