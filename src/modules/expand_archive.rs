f0aee41d7410a5282a43a0b7a2ec27b8d835e6a9
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-02-09 20:02:13 +0200

    testing archive expanding

---
+++ src/modules/expand_archive.rs
@@ -0,0 +1,46 @@
+use anyhow::{anyhow, Result};
+use futures_util::StreamExt;
+use tokio::fs;
+use tokio::io::AsyncWriteExt;
+
+pub async fn start() -> Result<()> {
+    expand_unix().await?;
+    Ok(())
+}
+
+#[cfg(target_family = "windows")]
+async fn expand_windows() -> Result<()> {
+    use async_zip::read::seek::ZipFileReader;
+
+    let mut file = fs::File::open("v0.6.1.zip").await?;
+    let mut zip = ZipFileReader::new(&mut file).await.unwrap();
+
+    for entry in zip.entries() {
+        let outpath = entry.name();
+
+        if entry.dir() {
+            fs::create_dir(outpath).await?;
+
+        } else {
+            let mut file = fs::File::create(outpath).await?;
+        }
+    }
+
+
+    Ok(())
+}
+
+#[cfg(target_family = "unix")]
+async fn expand_unix() -> Result<()> {
+    use async_tar::Archive;
+
+    let mut file = fs::File::open("v0.6.1.zip").await?;
+    let mut archive = Archive::new(file);
+    let mut entries = archive.entries()?;
+
+    while let Some(value) = entries.next().await {
+        let file = value?;
+        println!("{}", file.path().unwrap().display());
+    }
+    Ok(())
+}
--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -1,1 +1,2 @@
 pub mod use_handler;
+pub mod expand_archive;
