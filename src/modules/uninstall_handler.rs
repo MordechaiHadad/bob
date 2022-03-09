efa636ecefa889be5dd5a534af532f6cb05c4d1c
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-03-09 15:13:24 +0200

    added: tracing for logging

--- src/modules/uninstall_handler.rs
+++ src/modules/uninstall_handler.rs
@@ -3,6 +3,7 @@ use anyhow::{anyhow, Result};
 use reqwest::Client;
 use crate::modules::utils;
 use tokio::fs;
+use tracing::{info, warn};
 
 pub async fn start(subcommand: &ArgMatches) -> Result<()> {
     let client = Client::new();
@@ -17,7 +18,7 @@ pub async fn start(subcommand: &ArgMatches) -> Result<()> {
 
 
     if utils::is_version_used(&version).await {
-        println!("Switch to a different version before proceeding");
+        warn!("Switch to a different version before proceeding");
         return Ok(());
     }
 
@@ -27,6 +28,6 @@ pub async fn start(subcommand: &ArgMatches) -> Result<()> {
     };
 
     fs::remove_dir_all(&format!("{}/{version}", downloads_dir.display())).await?;
-    println!("Successfully uninstalled version: {version}");
+    info!("Successfully uninstalled version: {version}");
    Ok(())
 }
