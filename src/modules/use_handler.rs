86946acca998b843344a34598b7a410165407381
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-03-31 14:56:29 +0300

    added:(use command): Automatically add neovim bin to PATH

--- src/modules/use_handler.rs
+++ src/modules/use_handler.rs
@@ -4,6 +4,7 @@ use reqwest::Client;
 use crate::modules::{install_handler, utils};
 use tokio::fs;
 use tracing::info;
+use winreg::RegKey;
 
 pub async fn start(version: &str, client: &Client) -> Result<()> {
     if let Err(error) = install_handler::start(version, client, true).await {
@@ -12,7 +13,7 @@ pub async fn start(version: &str, client: &Client) -> Result<()> {
     if let Err(error) = link_version(version).await {
         return Err(anyhow!(error));
     }
-    info!("You can now open neovim");
+    info!("Neovim has successfully installed!");
     Ok(())
 }
 
@@ -35,16 +36,18 @@ async fn link_version(version: &str) -> Result<()> {
     cfg_if::cfg_if! {
         if #[cfg(windows)] {
            use std::os::windows::fs::symlink_dir;
+            use winreg::enums::*;
+
             let base_dir = if fs::metadata(&format!("{base_path}/Neovim")).await.is_ok() {
                 "Neovim"
             } else {
                 "nvim-win64"
             };
 
-           if symlink_dir(format!("{base_path}/{base_dir}"),
+            if symlink_dir(format!("{base_path}/{base_dir}"),
                format!("{}/neovim", installation_dir.display())).is_err() {
                    return Err(anyhow!("Please restart this application as admin to complete the installation."));
-               }
+            }
         } else {
             use std::os::unix::fs::symlink;
             let folder_name = if cfg!(target_os = "macos") {
@@ -61,10 +64,26 @@ async fn link_version(version: &str) -> Result<()> {
     info!("Linked {version} to {}/neovim", installation_dir.display());
 
     if !utils::is_version_used(version).await {
-        info!(
-            "Add {}/neovim/bin to PATH to complete this installation",
-            installation_dir.display()
-        ); // TODO: do this automatically
+        cfg_if::cfg_if!{
+            if #[cfg(windows)] {
+                use winreg::enums::*;
+
+                let current_usr = RegKey::predef(HKEY_CURRENT_USER);
+                let env = current_usr.open_subkey_with_flags("Environment", KEY_READ | KEY_WRITE)?;
+                let usr_path: String = env.get_value("Path")?;
+                let new_path = if usr_path.ends_with(';') {
+                    format!("{usr_path}{}\\neovim\\bin", installation_dir.display())
+                } else {
+                    format!("{usr_path};{}\\neovim\\bin", installation_dir.display())
+                };
+                env.set_value("Path", &new_path)?;
+            } else {
+                info!(
+                    "Add {}/neovim/bin to PATH to complete this installation",
+                    installation_dir.display()
+                );
+            }
+        }
     }
     Ok(())
 }
