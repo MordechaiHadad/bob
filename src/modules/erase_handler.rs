e2d52f0260294a531e41adf424bd4a9ae43213b6
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-04-13 18:30:42 +0300

    added: fully working erase command

--- src/modules/erase_handler.rs
+++ src/modules/erase_handler.rs
@@ -1,20 +1,31 @@
 use super::utils;
 use anyhow::Result;
 use tokio::fs;
+use tracing::info;
 
 pub async fn start() -> Result<()> {
     let downloads = utils::get_downloads_folder().await?;
     let installation_dir = utils::get_installation_folder()?;
 
+
+    fs::remove_dir_all(&installation_dir).await?;
+    info!("Successfully remove neovim's installation folder");
     fs::remove_dir_all(downloads).await?;
-    fs::remove_dir_all(installation_dir).await?;
+    info!("Successfully remove neovim downloads folder");
+
 
     cfg_if::cfg_if! {
         if #[cfg(windows)] {
             use winreg::RegKey;
+            use winreg::enums::*;
+
             let current_usr = RegKey::predef(HKEY_CURRENT_USER);
             let env = current_usr.open_subkey_with_flags("Environment", KEY_READ | KEY_WRITE)?;
             let usr_path: String = env.get_value("Path")?;
+            let usr_path = usr_path.replace(&format!("{}\\bin", installation_dir.display()), "");
+            env.set_value("Path", &usr_path)?;
+
+            info!("Successfully removed neovim's installation PATH from registry");
         }
     }
 
