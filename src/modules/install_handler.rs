b0093f1be651f1571f7341fdc1538287e27c5b4a
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-10-13 17:28:11 +0300

    added(build from source): Added some of the windows related code

--- src/modules/install_handler.rs
+++ src/modules/install_handler.rs
@@ -152,7 +152,21 @@ async fn download_version(
                 Err(error) => Err(anyhow!(error)),
             }
         }
-        VersionType::Hash => {
+        VersionType::Hash => handle_building_from_source(version, config).await,
+    }
+}
+
+async fn handle_building_from_source(
+    version: &InputVersion,
+    config: &Config,
+) -> Result<PostDownloadVersionType> {
+    cfg_if::cfg_if! {
+        if #[cfg(windows)] {
+            if let Err(_) = env::var("VisualStudioVersion") {
+                return Err("Please make sure you are using Developer PowerShell/Command Prompt for VS");
+            }
+
+        } else {
             let is_clang_present = match Command::new("clang").output().await {
                 Ok(_) => true,
                 Err(error) => match error.kind() {
@@ -173,67 +187,89 @@ async fn download_version(
                 ));
             }
 
-            match Command::new("cmake").output().await {
-                Ok(_) => (),
-                Err(error) => match error.kind() {
-                    std::io::ErrorKind::NotFound => {
-                        return Err(anyhow!(
-                            "Cmake has to be installed in order to build neovim from source"
-                        ))
-                    }
-                    _ => (),
-                },
-            }
+        }
 
-            let (mut child, is_installed) = if fs::metadata("neovim-git").await.is_err() {
-                // check if neovim-git
-                // directory exists
-                // to clone repo, else
-                // git pull changes
-                let child = match Command::new("git")
-                    .arg("clone")
-                    .arg("https://github.com/neovim/neovim")
-                    .arg("neovim-git")
-                    .spawn()
-                {
-                    Ok(value) => value,
-                    Err(error) => match error.kind() {
-                        std::io::ErrorKind::NotFound => {
-                            return Err(anyhow!(
-                                "Git has to be instaled in order to build neovim from source"
-                            ))
-                        }
-                        _ => return Err(anyhow!("Failed to clone neovim's repository")),
-                    },
-                };
-                (child, false)
-            } else {
-                env::set_current_dir("neovim-git")?; // cd into neovim-git
-                let child = match Command::new("git").arg("pull").spawn() {
-                    Ok(value) => value,
-                    Err(_) => return Err(anyhow!("Failed to pull upstream updates")),
-                };
-                (child, true)
-            };
-            child.wait().await?;
-            if !is_installed {
-                env::set_current_dir("neovim-git")?; // cd into neovim-git
+    }
+    match Command::new("cmake").output().await {
+        Ok(_) => (),
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => {
+                return Err(anyhow!(
+                    "Cmake has to be installed in order to build neovim from source"
+                ))
             }
-            Command::new("git")
-                .arg("checkout")
-                .arg(&version.tag_name)
-                .spawn()?
-                .wait()
-                .await?;
+            _ => (),
+        },
+    }
+    let (mut child, is_installed) = if fs::metadata("neovim-git").await.is_err() {
+        // check if neovim-git
+        // directory exists
+        // to clone repo, else
+        // git pull changes
+        let child = match Command::new("git")
+            .arg("clone")
+            .arg("https://github.com/neovim/neovim")
+            .arg("neovim-git")
+            .spawn()
+        {
+            Ok(value) => value,
+            Err(error) => match error.kind() {
+                std::io::ErrorKind::NotFound => {
+                    return Err(anyhow!(
+                        "Git has to be instaled in order to build neovim from source"
+                    ))
+                }
+                _ => return Err(anyhow!("Failed to clone neovim's repository")),
+            },
+        };
+        (child, false)
+    } else {
+        env::set_current_dir("neovim-git")?; // cd into neovim-git
+        let child = match Command::new("git").arg("pull").spawn() {
+            Ok(value) => value,
+            Err(_) => return Err(anyhow!("Failed to pull upstream updates")),
+        };
+        (child, true)
+    };
+    child.wait().await?;
+    if !is_installed {
+        env::set_current_dir("neovim-git")?; // cd into neovim-git
+    }
+    Command::new("git")
+        .arg("checkout")
+        .arg(&version.tag_name)
+        .spawn()?
+        .wait()
+        .await?;
 
-            if fs::metadata("build").await.is_ok() {
-                fs::remove_dir_all("build").await?;
+    if fs::metadata("build").await.is_ok() {
+        fs::remove_dir_all("build").await?;
+    }
+
+    let mut downloads_location = utils::get_downloads_folder(config).await?;
+    downloads_location.push(&version.tag_name[0..7]);
+    downloads_location.push(utils::get_platform_name());
+
+    cfg_if::cfg_if! {
+        if #[cfg(windows)] {
+            if fs::metadata(".deps").await.is_ok() {
+                fs::remove_dir_all(".deps").await?;
+                fs::create_dir(".deps").await?;
             }
+            env::set_current_dir(".deps")?;
+            Command::new("cmake").arg("../cmake.deps").spawn()?.wait().await?;
+            Command::new("cmake").arg("--build").arg(".").spawn()?.wait().await?;
 
-            let mut downloads_location = utils::get_downloads_folder(config).await?;
-            downloads_location.push(&version.tag_name[0..7]);
-            downloads_location.push(utils::get_platform_name());
-            let location_arg = format!("CMAKE_INSTALL_PREFIX={}", downloads_location.to_string_lossy());
+            let current_dir = env::current_dir()?;
+            let parent = current_dir.parent()?;
+
+            info!("Current directory: {}", env::current_dir().unwrap().display());
+
+        } else {
+            let location_arg = format!(
+                "CMAKE_INSTALL_PREFIX={}",
+                downloads_location.to_string_lossy()
+            );
             Command::new("make")
                 .arg(&location_arg)
                 .arg("CMAKE_BUILD_TYPE=RelWithDebInfo")
@@ -241,11 +277,10 @@ async fn download_version(
                 .wait()
                 .await?;
             Command::new("make").arg("install").spawn()?.wait().await?;
-
-            info!("Successfully installed {}", version.tag_name);
-            Ok(PostDownloadVersionType::Hash)
         }
     }
+    info!("Successfully installed {}", version.tag_name);
+    Ok(PostDownloadVersionType::Hash)
 }
 
 async fn send_request(client: &Client, version: &str) -> Result<reqwest::Response, reqwest::Error> {
