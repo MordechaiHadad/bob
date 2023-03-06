346cbda070ae6c798c24e162b20ec1c5b3059f9d
MordechaiHadad <mordechai.hadad01@gmail.com>
2023-03-06 18:00:36 +0200

    fix: rollback forgot to add a property

--- src/handlers/rollback_handler.rs
+++ src/handlers/rollback_handler.rs
@@ -47,6 +47,7 @@ pub async fn start(config: Config) -> Result<()> {
                 &ParsedVersion {
                     tag_name: name_list[i].clone(),
                     version_type: crate::helpers::version::types::VersionType::Standard,
+                    non_parsed_string: "".to_string()
                 },
             )
             .await?;
