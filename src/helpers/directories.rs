eaafe260d1b2d3899b4484807415cb0760e3fd97
MordechaiHadad <mordechai.hadad01@gmail.com>
2023-03-09 17:09:39 +0200

    add: debug print statement

--- src/helpers/directories.rs
+++ src/helpers/directories.rs
@@ -62,6 +62,7 @@ pub async fn get_downloads_directory(config: &Config) -> Result<PathBuf> {
             let mut data_dir = get_local_data_dir()?;
 
             data_dir.push("bob");
+            println!("{}", data_dir.display());
             let does_folder_exist = tokio::fs::metadata(&data_dir).await.is_ok();
 
             if !does_folder_exist && tokio::fs::create_dir(&data_dir).await.is_err() {
