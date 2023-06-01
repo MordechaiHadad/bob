7d374e34ac6344bc5f988f852a40c1fee64f56d3
MordechaiHadad <mordechai.hadad01@gmail.com>
2023-06-01 17:07:55 +0300

    added(update cmd): checks to see if specified version is installed before updating

--- src/handlers/update_handler.rs
+++ src/handlers/update_handler.rs
@@ -1,22 +1,38 @@
+use crate::helpers::version::is_version_installed;
 use crate::{cli::Update, config::Config};
 use anyhow::Result;
 use reqwest::Client;
-use tracing::info;
+use tracing::{info, warn};
 
 use super::{install_handler, InstallResult};
 
 pub async fn start(data: Update, client: &Client, config: Config) -> Result<()> {
     if data.all {
+        let mut did_update = false;
+
         let mut stable = crate::version::parse_version_type(client, "stable").await?;
-        match install_handler::start(&mut stable, client, &config).await? {
-            InstallResult::VersionAlreadyInstalled => info!("Stable is already updated!"),
-            InstallResult::InstallationSuccess(_) | InstallResult::NightlyIsUpdated => (),
+        if is_version_installed(&stable.tag_name, &config).await? {
+            match install_handler::start(&mut stable, client, &config).await? {
+                InstallResult::VersionAlreadyInstalled => info!("Stable is already updated!"),
+                InstallResult::InstallationSuccess(_) | InstallResult::NightlyIsUpdated => (),
+            }
+            did_update = true;
         }
 
-        let mut nightly = crate::version::parse_version_type(client, "nightly").await?;
-        match install_handler::start(&mut nightly, client, &config).await? {
-            InstallResult::NightlyIsUpdated => info!("Nightly is already updated!"),
-            InstallResult::InstallationSuccess(_) | InstallResult::VersionAlreadyInstalled => (),
+        if is_version_installed("nightly", &config).await? {
+            let mut nightly = crate::version::parse_version_type(client, "nightly").await?;
+            match install_handler::start(&mut nightly, client, &config).await? {
+                InstallResult::NightlyIsUpdated => info!("Nightly is already updated!"),
+                InstallResult::InstallationSuccess(_) | InstallResult::VersionAlreadyInstalled => {
+                    ()
+                }
+            }
+
+            did_update = true;
+        }
+
+        if !did_update {
+            warn!("There was nothing to update.");
         }
 
         return Ok(());
@@ -24,6 +40,10 @@ pub async fn start(data: Update, client: &Client, config: Config) -> Result<()> 
 
     let mut version = crate::version::parse_version_type(client, &data.version.unwrap()).await?;
 
+    if !is_version_installed(&version.tag_name, &config).await? {
+        warn!("{} is not installed.", version.non_parsed_string);
+        return Ok(());
+    } 
     match install_handler::start(&mut version, client, &config).await? {
         InstallResult::NightlyIsUpdated => info!("Nightly is already updated!"),
         InstallResult::VersionAlreadyInstalled => info!("Stable is already updated!"),
