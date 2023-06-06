31d9138f0ee396b5879569c8a86af17c8d2db49b
MordechaiHadad <mordechai.hadad01@gmail.com>
2023-06-06 14:55:15 +0300

    change: sync handle_subprocess to be conditionally "existing"

--- src/helpers/sync.rs
+++ src/helpers/sync.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, Result};
 use std::process::{Command, Stdio};
 
+#[cfg(target_os = "linux")]
 pub fn handle_subprocess(process: &mut Command) -> Result<()> {
     match process.stdout(Stdio::null()).status()?.code() {
         Some(0) => Ok(()),
