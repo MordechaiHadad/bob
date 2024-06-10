7c2a237639e0e974c05f24a670b556cdf5fd1358
morde <mordechai.hadad01@gmail.com>
2024-06-10 11:38:21 +0300

    Allow user to update all without using --all flag

--- src/handlers/update_handler.rs
+++ src/handlers/update_handler.rs
@@ -7,30 +7,27 @@ use tracing::{info, warn};
 use super::{install_handler, InstallResult};
 
 pub async fn start(data: Update, client: &Client, config: Config) -> Result<()> {
-    if data.all {
+    if data.version.is_none() || data.all {
         let mut did_update = false;
 
         let mut stable = crate::version::parse_version_type(client, "stable").await?;
         if is_version_installed(&stable.tag_name, &config).await? {
             match install_handler::start(&mut stable, client, &config).await? {
-                InstallResult::VersionAlreadyInstalled => info!("Stable is already updated!"),
-                InstallResult::InstallationSuccess(_)
+                InstallResult::InstallationSuccess(_) => did_update = true,
+                InstallResult::VersionAlreadyInstalled
                 | InstallResult::NightlyIsUpdated
                 | InstallResult::GivenNightlyRollback => (),
             }
-            did_update = true;
         }
 
         if is_version_installed("nightly", &config).await? {
             let mut nightly = crate::version::parse_version_type(client, "nightly").await?;
             match install_handler::start(&mut nightly, client, &config).await? {
-                InstallResult::NightlyIsUpdated => info!("Nightly is already updated!"),
-                InstallResult::InstallationSuccess(_)
+                InstallResult::InstallationSuccess(_) => did_update = true,
+                InstallResult::NightlyIsUpdated 
                 | InstallResult::VersionAlreadyInstalled
                 | InstallResult::GivenNightlyRollback => (),
             }
-
-            did_update = true;
         }
 
         if !did_update {
