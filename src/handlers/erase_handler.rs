27458b2645a734845a4b0e888618aca49f96c7fe
morde <mordechai.hadad01@gmail.com>
2024-06-13 14:10:12 +0300

    change: remove installation directory only if instalaltion location isnt specified in config

--- src/handlers/erase_handler.rs
+++ src/handlers/erase_handler.rs
@@ -6,11 +6,12 @@ use crate::{config::Config, helpers::directories};
 
 pub async fn start(config: Config) -> Result<()> {
     let downloads = directories::get_downloads_directory(&config).await?;
-    let mut installation_dir = directories::get_installation_directory(&config).await?;
-    installation_dir.push("nvim-bin");
+    let installation_dir = directories::get_installation_directory(&config).await?;
 
-    if fs::remove_dir_all(&installation_dir).await.is_ok() {
-        info!("Successfully removed neovim's installation folder");
+    if config.installation_location.is_none() {
+        if fs::remove_dir_all(&installation_dir).await.is_ok() {
+            info!("Successfully removed neovim's installation folder");
+        }
     }
     if fs::remove_dir_all(downloads).await.is_ok() {
         // For some weird reason this check doesn't really work for downloads folder
