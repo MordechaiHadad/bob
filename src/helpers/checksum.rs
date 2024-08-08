d6cf6b156001d2f3512fd42eb628a6402ca07a63
morde <mordechai.hadad01@gmail.com>
2024-08-08 15:01:21 +0300

    added: further debugging info

--- src/helpers/checksum.rs
+++ src/helpers/checksum.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use sha2::{Digest, Sha256};
+use tracing::info;
 use std::path::Path;
 use std::{fs, io};
 
@@ -14,15 +15,19 @@ use std::{fs, io};
 /// This function returns a `Result` that contains a `bool` indicating whether the checksum of the file at path 'a' matches the checksum saved in the file at path 'b'.
 /// If there is an error opening or reading the files, the function returns `Err(error)`.
 pub fn sha256cmp(a: &Path, b: &Path) -> Result<bool> {
+    info!("Checking checksum of file at path: {:?}", a);
     let checksum = fs::read_to_string(b)?;
     let checksum = checksum.split(' ').next().unwrap();
 
     let mut hasher = Sha256::new();
     let mut file = fs::File::open(a)?;
     io::copy(&mut file, &mut hasher)?;
+    info!("Checksum of file at path {:?} is: {:?}", a, hasher);
 
     let hash = hasher.finalize();
     let hash = format!("{:x}", hash);
+
+    info!("Checksum of file at path {:?} is: {:?}", a, hash);
 
     Ok(hash == checksum)
 }
