f5d34761da864b88f15e3f23908c2d6abcfc5bd1
MordechaiHadad <mordechai.hadad01@gmail.com>
2024-11-07 14:12:27 +0200

    fix: alignment of descriptions for invalid version string

--- src/helpers/version/mod.rs
+++ src/helpers/version/mod.rs
@@ -113,8 +113,8 @@ pub async fn parse_version_type(client: &Client, version: &str) -> Result<Parsed
                 "Please provide a proper version string. Valid options are:
 
                     • stable|latest|nightly - Latest stable, most recent, or nightly build
-                    • [v]x.x.x             - Specific version (e.g., 0.6.0 or v0.6.0)
-                    • <commit-hash>        - Specific commit hash"
+                    • [v]x.x.x              - Specific version (e.g., 0.6.0 or v0.6.0)
+                    • <commit-hash>         - Specific commit hash"
             ))
         }
     }
